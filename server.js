346232965f6219d4cc0890d3baf77d07f6956012
Nikhil Srivastava <Nikhilsrivastava0070@gmail.com>
2020-12-11 10:34:22 +0000

    4th commit

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const express = require('express');
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
-
+var { getinfo} = require("./service");
 // App
-const app = express();
+const app = express.json();
 app.get('/', (req, res) => {
   res.send('Welcome, This is Donkey');
 });
+app.get('/:id',getinfo)
 // App
 app.get('/',(req, res) => {
   res.sendFile(__dirname + '/index.html')
