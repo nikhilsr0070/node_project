2af8e1f01f9c831cba4d60173ca332d8bfac57cc
nikhilsr0070 <60070548+nikhilsr0070@users.noreply.github.com>
2021-02-11 11:40:05 +0530

    update file

--- file.py
+++ file.py
@@ -1,8 +1,7 @@
-echo "${"output"}"| python <(cat <<-EOF
+
 import sys, json
 full_def = json.load(sys.stdin)
 definition = full_def['taskDefinition']['containerDefinitions']
 definition[0]['image'] = '$REPOSITORY_URI:$TAG'
 print(json.dumps(definition))
-EOF
-  )
+  
