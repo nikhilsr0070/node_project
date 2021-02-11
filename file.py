53f4898d480e74f8ebfc3a2803afc14b30bc186f
nikhilsr0070 <60070548+nikhilsr0070@users.noreply.github.com>
2021-02-11 11:50:02 +0530

    updated

--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 
 import sys, json
-full_def = json.load(sys.stdin)
+import simplejson
+full_def = simplejson.load(sys.stdin)
 definition = full_def['taskDefinition']['containerDefinitions']
 definition[0]['image'] = '$REPOSITORY_URI:$TAG'
 print(json.dumps(definition))
