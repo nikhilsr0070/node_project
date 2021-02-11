e837df48c2e8fb435a2e27ebe16521d3d465fddb
nikhilsr0070 <60070548+nikhilsr0070@users.noreply.github.com>
2021-02-11 11:07:49 +0530

    Update file

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
-echo "${PREVIOUS_CONTAINER_DEF}" | python <(cat <<-EOF
+echo $output| python <(cat <<-EOF
 import sys, json
 full_def = json.load(sys.stdin)
 definition = full_def['taskDefinition']['containerDefinitions']
