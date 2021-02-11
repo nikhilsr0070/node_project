0e3180228807c06261379cbc372f669f60608675
nikhilsr0070 <60070548+nikhilsr0070@users.noreply.github.com>
2021-02-11 11:09:28 +0530

    Update jenkins

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
-echo $output| python <(cat <<-EOF
+echo "${"output"}"| python <(cat <<-EOF
 import sys, json
 full_def = json.load(sys.stdin)
 definition = full_def['taskDefinition']['containerDefinitions']
