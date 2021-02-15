d08584e107d177d6bcc81ed3656ce26ab298666f
nikhilsr0070 <60070548+nikhilsr0070@users.noreply.github.com>
2021-02-15 11:13:43 +0530

    Delete file

--- file.py
+++
@@ -1,8 +0,0 @@
-
-import sys, json
-import simplejson
-full_def = simplejson.load(sys.stdin)
-definition = full_def['taskDefinition']['containerDefinitions']
-definition[0]['image'] = '$REPOSITORY_URI:$TAG'
-print(json.dumps(definition))
-  
